a3d19e388d8091e73d2546066f8cc5baf88ee6d6
Pete Ridley <pete.ridley@outlook.com>
2021-04-26 15:34:40 +0100

    push222

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,6 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Menu />
-
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
